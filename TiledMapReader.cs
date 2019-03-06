0730dc212797f342a889a986553931bc810b303b
Jonah Elso-Mathews <jonahsol.e.m@gmail.com>
2019-03-06 14:12:50 +1100

    Modify TiledMapReader to add tiles & objects as entities with the required components

--- TiledMapReader.cs
+++ TiledMapReader.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using TiledSharp;
-
 
 using EntityComponentSystem;
 
@@ -17,167 +15,138 @@ namespace Rpg
     {
 
         /// <summary>
-        /// Add tiles param 'map' (read from a .tmx) to param 'ecs', where each tile is an Entity 
-        /// with relevant components.
+        /// Create entities from map editor tiles and objects.
         /// </summary>
-        /// <param name="entCollection"></param>
+        /// <param name="ecs"></param>
         /// <param name="map"></param>
         /// <param name="tilesetTxtr"></param>
         public static void AddTiledMapEntities(EntityComponentStorage ecs, TmxMap map, Texture2D tilesetTxtr)
         {
-            var entCollection = new List<Entity>();
-
+            // sprite sheet specs
             int? tilesetNumCols = map.Tilesets[0].Columns;
             int tileWidth = map.Tilesets[0].TileWidth;
             int tileHeight = map.Tilesets[0].TileHeight;
 
-            Vector2 mapTileLoc = new Vector2();
-            // tiles rotate around their centre
-            Vector2 origin = new Vector2(tileWidth / 2, tileWidth / 2);                                                        ///////////// can remove this
-            float rotation = 0f;
-
-            for (int iLayer = 0; iLayer < map.Layers.Count; iLayer++)
-            {
-                Collection<TmxLayerTile> curLayer = map.Layers[iLayer].Tiles;
-
-                for (var curTile = 0; curTile < curLayer.Count; curTile++)
-                {
-                    // gid identifies tile number eg. gid = 1 is top left tile of sprite sheet
-                    if (curLayer[curTile].Gid != 0)
-                    {
-                        if (curLayer[curTile].DiagonalFlip)
-                        {
-                            rotation = (float)(-90 * (Math.PI / 180));
-                        }
-                        else
-                        {
-                            rotation = 0;
-                        }
-
-                        if (curLayer[curTile].HorizontalFlip)
-                        {
-                            rotation += (float)(180 * (Math.PI / 180));
-                        }
-
-                        if (curLayer[curTile].VerticalFlip)
-                        {
-                            //rotation -= (float)(180 * (Math.PI / 180));
-                        }
-
-                        // calculate location of sprite in sprite sheet
-                        int curTileImgOffset = curLayer[curTile].Gid - 1;
-                        int tilesetLocX = (int)(curTileImgOffset % tilesetNumCols);
-                        int tilesetLocY = (int)(curTileImgOffset / tilesetNumCols);
-                        Rectangle tilesetRect = new Rectangle(tilesetLocX * tileWidth,
-                                                              tilesetLocY * tileHeight,
-                                                              tileWidth,
-                                                              tileHeight
-                                                             );
-
-                        // calculate in game position (first tile has position 0,0)
-                        float x = (curTile % map.Width) * tileWidth;
-                        float y = (float)(curTile / map.Height) * tileHeight;
-                        mapTileLoc.X = x;
-                        mapTileLoc.Y = y;
-
-                        ecs.AddEntity(new RenderComponent(ecs, tilesetTxtr, tilesetRect, rotation),
-                                      new PositionComponent(ecs, mapTileLoc),
-                                      new CollisionComponent(ecs, Vector2.Zero, new Vector2(tileWidth, tileHeight)));
-
-                        // render current tile to screen
-                        //spriteBatch.Draw(tilesetTxtr, mapTileLoc, tilesetRect, Color.White,
-                        //                            rotation, origin, 1.0f, SpriteEffects.None, 0f);
-                    }
-                }
-            }
-        }
-
-        /// <summary> 
-        /// Uses TiledSharp api to draw Tiled map specified by <paramref name="map"/>.
-        /// </summary>
-        /// <param name="map"> 
-        /// Set of tiled grids, each tile has an id tag, which is a reference to 
-        /// a sprite in <see paramref name="tilesetTxtr"/>
-        /// </param>
-        /// <param name="tilesetTxtr"> games spritesheet </param> 
-        /// <param name="spriteBatch"> monogame api for rendering to screen</param>
-        public static void DrawMap(TmxMap map, Texture2D tilesetTxtr, SpriteBatch spriteBatch)
-        {
-            int? tilesetNumCols = map.Tilesets[0].Columns;
-            int tileWidth = map.Tilesets[0].TileWidth;
-            int tileHeight = map.Tilesets[0].TileHeight;
-
-            Vector2 mapTileLoc = new Vector2();
+            // declare objects for render components
+            Vector2 mapLoc = new Vector2();
             // tiles rotate around their centre
             Vector2 origin = new Vector2(tileWidth / 2, tileWidth / 2);
             float rotation = 0f;
 
-            for (int iObjGroup = 0; iObjGroup < map.ObjectGroups.Count; iObjGroup++)
+            // add tmx objects as entities with position and render components, as well as 
+            // other components determined by the object type
+            for (int iObjectGroup = 0; iObjectGroup < map.ObjectGroups.Count; iObjectGroup++)
             {
-                Collection<TmxObject> curObjGroup = map.ObjectGroups[iObjGroup].Objects;
-
-                for (var curObj = 0; curObj < curObjGroup.Count; curObj++)
+                for (int iObject = 0; iObject < map.ObjectGroups[iObjectGroup].Objects.Count; iObject++)
                 {
+                    TmxObject obj = map.ObjectGroups[iObjectGroup].Objects[iObject];
 
+                    // it appeared that: obj.Tile.X == obj.Tile.Y != obj.Y; causing entities to be
+                    // rendered in the wrong position. 'createComponentsFromTile' modified to 
+                    // optionally accept position arguments
+                    (RenderComponent renderComp, PositionComponent posComp) =
+                        createComponentsFromTile(obj.Tile, (int)obj.X, (int)obj.Y);
+
+
+                    if (obj.Type == "Collision")
+                    {
+                        Entity newEnt = ecs.AddEntity(renderComp, posComp,
+                                new CollisionComponent(ecs, Vector2.Zero, new Vector2(tileWidth, tileHeight)));
+                    }
+                    else
+                    {
+                        Entity newEnt = ecs.AddEntity(renderComp, posComp);
+                    }
                 }
-              
-
             }
 
+            // add tmx tiles (just sprite with location) as entities with position and render 
+            // components
             for (int iLayer = 0; iLayer < map.Layers.Count; iLayer++)
             {
                 Collection<TmxLayerTile> curLayer = map.Layers[iLayer].Tiles;
 
                 for (var curTile = 0; curTile < curLayer.Count; curTile++)
                 {
-                    // gid identifies tile number eg. gid = 1 is top left tile of sprite sheet
+                    // gid identifies sprite location in sheet eg. gid = 1 is top left tile of sprite sheet
                     if (curLayer[curTile].Gid != 0)
                     {
-                        if (curLayer[curTile].DiagonalFlip)
+                        (RenderComponent renderComp, PositionComponent posComp) = 
+                            createComponentsFromTile(curLayer[curTile], null, null);
+
+                        if (renderComp != null && posComp != null)
                         {
-                            rotation = (float)(-90 * (Math.PI / 180));
+                            ecs.AddEntity(renderComp, posComp);
                         }
-                        else
-                        {
-                            rotation = 0;
-                        }
+                        
+                    }
+                }
+            }
 
-                        if (curLayer[curTile].HorizontalFlip)
-                        {
-                            rotation += (float)(180 * (Math.PI / 180));
-                        }
-
-                        if (curLayer[curTile].VerticalFlip)
-                        {
-                            //rotation -= (float)(180 * (Math.PI / 180));
-                        }
-
-                        // calculate location of sprite in sprite sheet
-                        int curTileImgOffset = curLayer[curTile].Gid - 1;
-                        int tilesetLocX = (int)(curTileImgOffset % tilesetNumCols);
-                        int tilesetLocY = (int)(curTileImgOffset / tilesetNumCols);
-                        Rectangle tilesetRect = new Rectangle(tilesetLocX * tileWidth,
-                                                              tilesetLocY * tileHeight,
-                                                              tileWidth,
-                                                              tileHeight
-                                                             );
-
-                        // calculate rectangle to display sprite on screen
-                        float x = (curTile % map.Width) * tileWidth;
-                        float y = (float)(curTile / map.Height) * tileHeight;
-                        mapTileLoc.X = x;
-                        mapTileLoc.Y = y;
-
-                        // render current tile to screen
-                        spriteBatch.Draw(tilesetTxtr, mapTileLoc, tilesetRect, Color.White,
-                                                    rotation, origin, 1.0f, SpriteEffects.None, 0f);
-
+            // Create render and position components for a given tile. 
+            //
+            // If arguments passed to x & y are non-null, then they will be used for the position, 
+            // component. Otherwise parameter position from parameter 'tile' will be used.
+            // x & y arguments should be in pixels.
+            (RenderComponent, PositionComponent) createComponentsFromTile(TmxLayerTile tile, 
+                int? x, int? y)
+            {
+                // gid identifies sprite location in sheet eg. gid = 1 is top left tile of sprite sheet
+                if (tile.Gid != 0)
+                {
+                    if (tile.DiagonalFlip)
+                    {
+                        rotation = (float)(-90 * (Math.PI / 180));
+                    }
+                    else
+                    {
+                        rotation = 0;
                     }
 
-                }
+                    if (tile.HorizontalFlip)
+                    {
+                        rotation += (float)(180 * (Math.PI / 180));
+                    }
 
+                    if (tile.VerticalFlip)
+                    {
+                        //rotation -= (float)(180 * (Math.PI / 180));
+                    }
+
+                    // calculate location of sprite in sprite sheet
+                    int curTileImgOffset = tile.Gid - 1;
+                    int tilesetLocX = (int)(curTileImgOffset % tilesetNumCols);
+                    int tilesetLocY = (int)(curTileImgOffset / tilesetNumCols);
+                    Rectangle tilesetRect = new Rectangle(tilesetLocX * tileWidth,
+                                                          tilesetLocY * tileHeight,
+                                                          tileWidth,
+                                                          tileHeight
+                                                         );
+
+                    // optionally accept x & y args.
+                    if (x != null && y != null)
+                    {
+                        mapLoc.X = (float)x;
+                        mapLoc.Y = (float)y;
+                    }
+                    else
+                    {
+                        // multiply since 'TmxLayerTile' x & y properties are tile positions, 
+                        // not pixels
+                        mapLoc.X = tile.X * tileWidth;
+                        mapLoc.Y = tile.Y * tileHeight;
+                    }
+                 
+
+                    return (new RenderComponent(ecs, tilesetTxtr, tilesetRect, rotation),
+                            new PositionComponent(ecs, mapLoc)
+                           );
+                }
+                else
+                {
+                    return (null, null);
+                }
             }
         }
-
     }
 }
